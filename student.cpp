d76b1080eed0d0161affe36748ee4bae9350c13f
Buster Holzbauer <holzbh@cs.rpi.edu>
2018-06-29 18:25:31 -0400

    Fixed percentages. Not sure how a 0 point, all EC category could work, despite existing comment about it, so just ignoring that problem for now.

--- student.cpp
+++ student.cpp
@@ -127,24 +127,36 @@ float Student::GradeablePercent(GRADEABLE_ENUM g) const {
     return lowest_test_counts_half_pct();
   }
 
-  // collect the scores in a vector
-  std::vector<score_object> scores;
-  bool nonzero_gradeable = false;
+  //Do one pass to get the defined item scores
+  float nonzero_sum = 0;
+  int nonzero_count = 0;
   for (int i = 0; i < GRADEABLES[g].getCount(); i++) {
     float s = getGradeableItemGrade(g,i).getValue();
     std::string id = GRADEABLES[g].getID(i);
-    float m = GRADEABLES[g].getItemMaximum(id);
-    float p = GRADEABLES[g].getItemPercentage(id);
-    float sm = GRADEABLES[g].getScaleMaximum(id);
-    scores.push_back(score_object(s,m,p,sm));
-    if (m > 0){
-      nonzero_gradeable = true;
-    }
+    if(!id.empty()){
+      nonzero_sum += GRADEABLES[g].getItemMaximum(id);
+      nonzero_count++;
+    }    
   }
 
   //If there are no gradeables with a max >0, bucket is 0% anyway
-  if(!nonzero_gradeable){
+  //XXX: Not sure if this is going to break totally extra-credit categories. Test this.
+  if(nonzero_count == 0){
     return 0.0;
+  }
+
+  // collect the scores in a vector
+  std::vector<score_object> scores;
+  for (int i = 0; i < GRADEABLES[g].getCount(); i++) {
+    float s = getGradeableItemGrade(g,i).getValue();
+    std::string id = GRADEABLES[g].getID(i);
+    float m = nonzero_sum/nonzero_count;
+    if(!id.empty()){
+      m = GRADEABLES[g].getItemMaximum(id);
+    }
+    float p = GRADEABLES[g].getItemPercentage(id);
+    float sm = GRADEABLES[g].getScaleMaximum(id);
+    scores.push_back(score_object(s,m,p,sm));
   }
 
   // sort the scores (smallest first)
@@ -170,9 +182,10 @@ float Student::GradeablePercent(GRADEABLE_ENUM g) const {
     float my_max = std::max(m,sm);
     if (p < 0) {
       //Skip any non-contributing gradeable
-      if (my_max <= 0){
+      /*if (my_max <= 0){
         continue;
-      }
+      }*/
+      assert(my_max > 0);
       if (sum_max > 0) {
         p = std::max(m,sm) / sum_max;
       } else {
