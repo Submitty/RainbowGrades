c2ef907fd7a95272ff03ef9668c08b36571aafa0
Jaeseok Kang <123261952+ziesski@users.noreply.github.com>
2023-06-27 14:08:59 -0400

    [Feature:RainbowGrades] Border outline reflects status (#51)
    fixes Submitty/Submitty#9112
    fixes Submitty/Submitty#9331 (EDIT (06.27.2023) - from student.cpp line
    474 implementing LookupGradeable etc.
    
    
    Rainbow Grade will now have border outline for 4 status.
    Black border outline = Academic integrity violation
    Yellow border outline = Grade override
    Green border outline = Grade inquiry in progress
    Read border outline = Bad status
     
    
    Works for both individual & instructor (transposed) version
    ![Screen Shot 2023-06-02 at 12 53 42
    PM](https://github.com/Submitty/RainbowGrades/assets/123261952/5e8fb9d3-41d1-40d0-aeec-546b38b0a38d)
    ![Screen Shot 2023-06-02 at 12 59 50
    PM](https://github.com/Submitty/RainbowGrades/assets/123261952/8efc7b45-3c14-4161-b5e9-a989444367fb)
    
    ---------
    
    Co-authored-by: Barb Cutler <bmcutler@users.noreply.github.com>

--- table.cpp
+++ table.cpp
@@ -72,16 +72,74 @@ TableCell::TableCell(const std::string& c, float d, int precision, const std::st
   rotate = 0;
 }
 
+
+TableCell::TableCell(float d, const std::string& c, int precision, const std::string& n, int ldu,
+                     CELL_CONTENTS_STATUS v,const std::string& e,bool ai, const std::string& a, int s, int /*r*/) {
+  assert (c.size() == 6);
+  assert (precision >= 0);
+  color=c;
+  if (fabs(d) > 0.0001) {
+    std::stringstream ss;
+    ss << std::setprecision(precision) << std::fixed << d;
+    data=ss.str(); span=s;
+  } else {
+    data = "";
+  }
+  note=n;
+  late_days_used=ldu,
+  visible=v;
+  align=a;
+  span=s;
+  rotate = 0;
+  academic_integrity = ai;
+  event = e;
+  if (event == "Bad"){
+    bad_status = true;
+    override = inquiry = false;
+  } else if ( event == "Overridden"){
+    override = true;
+    bad_status = inquiry = false;
+  } else if (event == "Open"){
+    inquiry = true;
+    bad_status = override = false;
+  } else {
+   inquiry = bad_status = override = false; 
+  }
+    
+}
+
+
+
 std::ostream& operator<<(std::ostream &ostr, const TableCell &c) {
   assert (c.color.size() == 6);
+    
+    std::string outline = "";
+    std::string mark = "";
+    if (c.academic_integrity){
+        outline = "outline:4px solid #0a0a0a; outline-offset: -4px;";
+        mark = "@";
+    } else if (c.override){
+        outline = "outline:4px solid #fcca03; outline-offset: -4px;";
+    } else if (c.inquiry){
+        outline = "outline:4px dashed #1cfc03; outline-offset: -4px;";
+    } else if (c.bad_status){
+        outline = "outline:4px solid #fc0303; outline-offset: -4px;";
+    }
+    
   //  ostr << "<td bgcolor=\"" << c.color << "\" align=\"" << c.align << "\">";
-  ostr << "<td style=\"border:1px solid #aaaaaa; background-color:#" << c.color << ";\" align=\"" << c.align << "\">";
+  ostr << "<td style=\"border:1px solid #aaaaaa; background-color:#" << c.color << "; " << outline << " \" align=\"" << c.align << "\">";
+    
   if (0) { //rotate == 90) {
     ostr << "<div style=\"position:relative\"><p class=\"rotate\">";
   }
   ostr << "<font size=-1>";
   std::string mynote = c.getNote();
-  if ((c.data == "" && mynote=="")
+
+  if (c.academic_integrity)
+  {
+    ostr << mark;
+  }
+  else if ((c.data == "" && mynote=="")
       || c.visible==CELL_CONTENTS_HIDDEN
       || (c.visible==CELL_CONTENTS_VISIBLE_INSTRUCTOR && GLOBAL_instructor_output == false) 
       || (c.visible==CELL_CONTENTS_VISIBLE_STUDENT    && GLOBAL_instructor_output == true)) {
@@ -92,6 +150,8 @@ std::ostream& operator<<(std::ostream &ostr, const TableCell &c) {
       if (c.late_days_used > 3) { ostr << " (" << std::to_string(c.late_days_used) << "*)"; }
       else { ostr << " " << std::string(c.late_days_used,'*'); }
     }
+      
+    
     if (mynote.length() > 0 &&
         mynote != " " &&
         (global_details 
@@ -177,10 +237,7 @@ void Table::output(std::ostream& ostr,
           ostr << "<em>Information last updated: " << last_update << "</em><br>\n";
       }
       ostr << "&nbsp;<br>\n";
-
-
       ostr << "<table style=\"border:1px solid #aaaaaa; background-color:#aaaaaa;\">\n";
-      //  ostr << "<table border=0 cellpadding=3 cellspacing=2 style=\"background-color:#aaaaaa\">\n";
   }
   
   if (transpose) {
