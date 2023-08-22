b4d84c6349e03d68220cbd424c7bb73c3986b417
Jaeseok Kang <ziesski@gmail.com>
2023-08-22 09:51:33 -0400

    change output

--- output.cpp
+++ output.cpp
@@ -1174,8 +1174,64 @@ void start_table_output( bool /*for_instructor*/,
 void end_table(std::ofstream &ostr,  bool for_instructor, Student *s) {
 
   ostr << "<p>* = 1 late day used</p>" << std::endl;
-  // This should only pop up for those who violated policy FIX HERE
-  // ostr << "<p>@ = Academic Integrity Violation</p>" << std::endl;
+
+  // Description of border outline that are in effect
+  if (s != NULL)
+  {
+      if (s->get_event_bad_status() || s->get_event_grade_inquiry() || s->get_event_overridden() || s->get_event_academic_integrity())
+      {
+        ostr << "<style> .spacer {display: inline-block; width: 66px;} </style>\n";
+        ostr << "<table style=\"border:1px solid #aaaaaa; background-color:#FFFFFF;\">\n";
+        if (s->get_event_academic_integrity())
+        {
+          ostr << "<tr>\n";
+          ostr << "<td style=\"border:1px solid #aaaaaa; background-color:#FFFFFF" << "; " << "outline:4px solid #0a0a0a; outline-offset: -4px;" << " \" align=\"" << "left" << "\">";
+          ostr << "<span class=\"spacer\"></span>";
+          ostr << "</td>";
+          ostr << "<td style=\"border:1px solid #aaaaaa; background-color:#FFFFFF" << "; " << " \" align=\"" << "left" << "\">";
+          ostr << "<font size = \"-1\"> Academic Integrity Violation </font>";
+          ostr << "</td>";
+          ostr << "</tr>\n";
+        }
+        if (s->get_event_overridden())
+        {
+          ostr << "<tr>\n";
+          ostr << "<td style=\"border:1px solid #aaaaaa; background-color:#FFFFFF" << "; " << "outline:4px solid #fcca03; outline-offset: -4px;" << " \" align=\"" << "left" << "\">";
+          ostr << "<span class=\"spacer\"></span>";
+          ostr << "</td>";
+          ostr << "<td style=\"border:1px solid #aaaaaa; background-color:#FFFFFF" << "; " << " \" align=\"" << "left" << "\">";
+          ostr << "<font size = \"-1\"> Grade override </font>";
+          ostr << "</td>";
+          ostr << "</tr>\n";
+        }
+        if (s->get_event_grade_inquiry())
+        {
+          ostr << "<tr>\n";
+          ostr << "<td style=\"border:1px solid #aaaaaa; background-color:#FFFFFF" << "; " << "outline:4px dashed #1cfc03; outline-offset: -4px;" << " \" align=\"" << "left" << "\">";
+          ostr << "<span class=\"spacer\"></span>";
+          ostr << "</td>";
+          ostr << "<td style=\"border:1px solid #aaaaaa; background-color:#FFFFFF" << "; " << " \" align=\"" << "left" << "\">";
+          ostr << "<font size = \"-1\"> Grade inquiry in progress </font>";
+          ostr << "</td>";
+          ostr << "</tr>\n";
+        }
+        if (s->get_event_bad_status())
+        {
+          ostr << "<tr>\n";
+          ostr << "<td style=\"border:1px solid #aaaaaa; background-color:#FFFFFF" << "; " << "outline:4px solid #fc0303; outline-offset: -4px;" << " \" align=\"" << "left" << "\">";
+          ostr << "<span class=\"spacer\"></span>";
+          ostr << "</td>";
+          ostr << "<td style=\"border:1px solid #aaaaaa; background-color:#FFFFFF" << "; " << " \" align=\"" << "left" << "\">";
+          ostr << "<font size = \"-1\"> Bad status = too many late days used on this assignment <br> ";
+          ostr << "<span class=\"spacer\"></span> OR you didn’t have enough late days to use </font>";
+          ostr << "</td>";
+          ostr << "</tr>\n";
+        }
+        ostr << "</table>\n";
+      }
+  }
+
+
 
   if (s != NULL) {
     std::ifstream istr("student_poll_reports/"+s->getUserName()+".html");
