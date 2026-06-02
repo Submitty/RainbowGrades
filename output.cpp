88e9d4c30123c62f5f1c7a64af965dba57959a44
dagemcn <45247229+dagemcn@users.noreply.github.com>
2026-06-02 11:20:32 -0400

    [Feature:RainbowGrades] Link to gradeable (#88)
    Closes https://github.com/Submitty/Submitty/issues/10433
    
    Credit to @oliiso and @ryvaru for doing most of the work on this issue
    on https://github.com/Submitty/RainbowGrades/pull/81 and
    https://github.com/Submitty/RainbowGrades/pull/83
    
    What is the current behavior?
    Currently, students can only see their grade for a specific gradeable on
    Rainbow Grades, and would have to search through gradeables to see their
    full report.
    
    What is the new behavior?
    Now, when students see their grade on Rainbow Grades, they can just
    click on it and it sends them to see their full report directly. This is
    more convenient and helpful for students. Note that for a gradeable to
    have a link, the gradeable must be released AND its type must be an
    Electronic File Upload, nothing else will work.
    
    This version should work with manual setup as well as the automatic
    setup.
    
    <img width="810" height="1358" alt="image"
    src="https://github.com/user-attachments/assets/e7feac9c-b993-4f02-87fd-bf24e3578b03"
    />
    
    ---------
    
    Co-authored-by: Sophia Oliinik <sophia.oliinik@wallawalla.edu>
    Co-authored-by: Viane Matsibekker <04vmatsibekker@gmail.com>
    Co-authored-by: Eric McNitt <ericmcnitt@Erics-Air.lan>
    Co-authored-by: Barb Cutler <Barb Cutler>

--- output.cpp
+++ output.cpp
@@ -139,6 +139,48 @@ int convertMajor(const std::string &major) {
   else return 10;
 }
 
+std::tuple<std::string, std::string, std::string> getCourseDetails() {
+    std::ifstream i("./raw_data/base_url.json");
+    nlohmann::json j;
+    i >> j;
+    assert (j.find("base_url") != j.end());
+    std::string baseUrl = j["base_url"].get<std::string>();
+    assert (j.find("term") != j.end());
+    std::string term = j["term"].get<std::string>();
+    assert (j.find("course") != j.end());
+    std::string course = j["course"].get<std::string>();
+    return {baseUrl, term, course};
+}
+
+std::string getGradeableType(const std::string &firstUserName, const std::string &gradeableID) {
+    std::ifstream i( ("./raw_data/all_grades/" + firstUserName + "_summary.json") );
+    nlohmann::json j;
+    i >> j;
+
+    std::string gradeableType = "";
+
+    for (auto it = j.begin(); it != j.end(); ++it) {
+      if (!it.value().is_array()) {
+        continue;
+      }
+      for (const auto& item : it.value()) {
+        if (item.contains("id") && item["id"].is_string() && 
+          item["id"].get<std::string>() == gradeableID) {
+          if (item.contains("gradeable_type") && item["gradeable_type"].is_string()) {
+            gradeableType = item["gradeable_type"].get<std::string>();
+            break;
+          }
+        }
+      }
+
+      if (!gradeableType.empty()) {
+        break;
+      }
+    }
+
+  return gradeableType;
+}
+
 // ==========================================================
 
 class Color {
@@ -635,17 +677,43 @@ void start_table_output( bool /*for_instructor*/,
   // ----------------------------
   // DETAILS OF EACH GRADEABLE
   if (DISPLAY_GRADE_DETAILS) {
+    std::string firstUserName = "";
+    for(unsigned int stu = 0; stu < students.size(); stu++){
+      Student *this_student = students[stu];
+      if (!validSection(this_student->getSection())) continue;
+      else {
+        firstUserName = this_student->getUserName();
+        break;
+      }
+    }
+
     for (unsigned int i = 0; i < ALL_GRADEABLES.size(); i++) {
       GRADEABLE_ENUM g = ALL_GRADEABLES[i];
       for (int j = 0; j < GRADEABLES[g].getCount(); j++) {
         if (g != GRADEABLE_ENUM::NOTE) {
           student_data.push_back(counter);
         }
+
         std::string gradeable_id = GRADEABLES[g].getID(j);
         std::string gradeable_name = "";
+
+        auto courseDetails = getCourseDetails();
+        std::string base_url = std::get<0>(courseDetails);
+        std::string semester = std::get<1>(courseDetails);
+        std::string course = std::get<2>(courseDetails);
+        std::string fullUrl = base_url + "courses/" + semester + "/" + course + "/gradeable/" + gradeable_id;
+
+        std::string gradeableType = getGradeableType(firstUserName, gradeable_id);
+
         if (GRADEABLES[g].hasCorrespondence(gradeable_id)) {
           gradeable_name = GRADEABLES[g].getCorrespondence(gradeable_id).second;
+          bool checkReleased = GRADEABLES[g].isReleased(gradeable_id);
+          if(checkReleased && gradeableType == "Electronic File"){
+            gradeable_name = gradeable_name + " <i class='fa-solid fa-arrow-up-right-from-square' style='color:blue;'></i>";
+            gradeable_name = "<a href=\"" + fullUrl + "\" style=\"color:black; text-decoration:none;\" title=\"View Gradeable\">" + gradeable_name + "</a>";
+          }
           //gradeable_name = spacify(gradeable_name);
+          gradeable_name = "<a href=\"" + fullUrl + "\" style=\"color:black;\">" + gradeable_name + "&nbsp;&nbsp; <i class='fas fa-external-link-alt'></i></a>";
         }
         if (gradeable_name == "")
           gradeable_name = "<em><font color=\"aaaaaa\">future "
