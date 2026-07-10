3c301ee6ca49f6eb474292e32c56b4389cb9511a
dagemcn <d.eric.mcnitt@gmail.com>
2026-07-10 11:58:51 -0400

    fix seating bug

--- output.cpp
+++ output.cpp
@@ -427,7 +427,9 @@ void PrintExamRoomAndZoneTable(const std::string &g_id, nlohmann::json &mj, Stud
 
   // ==============================================================
 
-  if ( DISPLAY_EXAM_SEATING == false) return;
+  if (DISPLAY_EXAM_SEATING == false) return;
+
+  if (GRADEABLES[GRADEABLE_ENUM::TEST].getCount() == 0) return;
 
   std::string room = GLOBAL_EXAM_DEFAULT_ROOM;
   std::string building = GLOBAL_EXAM_DEFAULT_BUILDING;
